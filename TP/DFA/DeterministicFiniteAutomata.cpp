8b35bd63b332d09e5e1c65c5b51bdf5e6c1a3bf9
Ignacio Dias Gundin <ignadiasg@gmail.com>
2024-03-27 12:06:46 -0300

    class afnd

---
+++ TP/DFA/DeterministicFiniteAutomata.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DeterministicFiniteAutomata {
+private:
+    vector<int> K;
+    vector<int> E;
+    vector<tuple<int, int, vector<int>>> d;
+    int q0;
+    vector<int> F;
+
+
+};

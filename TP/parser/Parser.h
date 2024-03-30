f5109b5cec88a8e9d1859dd69dfc9b85ca07e0a7
Lara Combina <65500848+larchadrg@users.noreply.github.com>
2024-03-30 11:41:50 -0300

    Update Parser.h

--- TP/parser/Parser.h
+++ TP/parser/Parser.h
@@ -8,8 +8,23 @@ using  namespace std;
 class Parser {
 public:
     static void identifyFormat(const string& line);
+    Parser();
+        ~Parser();
+
+        bool validateFirstLine(std::string line);
+        bool validateLastLine(std::string line);
+        bool validateTransitionLine(std::string line);
+        bool validateInitialStateLine(std::string line);
+        bool validateFinalStateLine(std::string line);
+        bool isRankdirLine(std::string line);
+
+        void addInitialStateFromInitialStateLine(std::string line);
+        void addFinalStateFromFinalStateLine(std::string line);
+        void addAutomataInformationFromTransitionLine(std::string line);
+
 private:
     static bool checkDelta(const string& line);
+    NotDeterministicFiniteAutomata nda; 
 };
 
 

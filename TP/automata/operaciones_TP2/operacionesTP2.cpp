b9c111294bd40b61111a198f11d466bc4a7bff70
lcombina-mavin <lcombina@mavincompany.com>
2024-05-13 10:49:08 -0300

    headers para el tp2

---
+++ TP/automata/operaciones_TP2/operacionesTP2.cpp
@@ -0,0 +1,25 @@
+#include "operacionesTP2.h"
+
+NotDeterministicFiniteAutomata AutomataOperations::nfaUnion(NotDeterministicFiniteAutomata a1, NotDeterministicFiniteAutomata a2)
+{
+    //TODO
+    return NotDeterministicFiniteAutomata();
+}
+
+NotDeterministicFiniteAutomata AutomataOperations::nfaConcatenation(NotDeterministicFiniteAutomata a1, NotDeterministicFiniteAutomata a2)
+{
+    //TODO
+    return NotDeterministicFiniteAutomata();
+}
+
+NotDeterministicFiniteAutomata AutomataOperations::kleeneClosure(NotDeterministicFiniteAutomata a1, NotDeterministicFiniteAutomata a2)
+{
+    //TODO
+    return NotDeterministicFiniteAutomata();
+}
+
+DeterministicFiniteAutomata AutomataOperations::minimization(DeterministicFiniteAutomata dfa)
+{
+    //TODO
+    return DeterministicFiniteAutomata();
+}

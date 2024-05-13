6c9df6aa98451047f042e6ff7d5bd3168aea433b
lcombina-mavin <lcombina@mavincompany.com>
2024-05-13 11:18:54 -0300

    op union

--- TP/automata/operaciones_TP2/operacionesTP2.cpp
+++ TP/automata/operaciones_TP2/operacionesTP2.cpp
@@ -1,9 +1,41 @@
 #include "operacionesTP2.h"
+#define LAMBDA -1
+
 
 NotDeterministicFiniteAutomata AutomataOperations::nfaUnion(NotDeterministicFiniteAutomata a1, NotDeterministicFiniteAutomata a2)
 {
-    //TODO
-    return NotDeterministicFiniteAutomata();
+    NotDeterministicFiniteAutomata newAutomata = NotDeterministicFiniteAutomata();
+    
+    //generate new initial state
+    int sumLenghtAutomatas = a1.getSates().size() + a2.getSates().size(); 
+    int newInitialState = sumLenghtAutomatas;
+    while (a1.getSates().count(newInitialState) > 0 || a2.getSates().count(newInitialState) > 0) {
+            newInitialState++;
+    }
+
+    newAutomata.addState(newInitialState);
+    newAutomata.setInitialState(newInitialState); 
+
+    //add states of a1 and a2
+    for(int state: a1.getSates()){
+        newAutomata.addState(state); 
+    }
+    for(int state: a1.getSates()){
+        newAutomata.addState(state); 
+    }
+    //add paths from new state
+    newAutomata.addPath(newInitialState,LAMBDA,a1.getInitialState());
+    newAutomata.addPath(newInitialState,LAMBDA,a2.getInitialState());
+
+    //set final states
+    for(int state: a1.getFinalSates()){
+        newAutomata.addFinalState(state);
+    }
+    for(int state: a2.getFinalSates()){
+        newAutomata.addFinalState(state);
+    }
+    
+    return newAutomata; 
 }
 
 NotDeterministicFiniteAutomata AutomataOperations::nfaConcatenation(NotDeterministicFiniteAutomata a1, NotDeterministicFiniteAutomata a2)
